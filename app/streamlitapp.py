6d35e5e8d318e91bdabace5472416abfd0af6557
Chris <40270192+ChrisDavidson7@users.noreply.github.com>
2023-05-20 11:46:16 +0530

    Update streamlitapp.py

--- app/streamlitapp.py
+++ app/streamlitapp.py
@@ -16,7 +16,7 @@ with st.sidebar:
     st.image('https://www.onepointltd.com/wp-content/uploads/2020/03/inno2.png')
     st.title('SilentScribe')
     st.info('SilentScribe is inspired by the LipNet deep learning model.')
-    st.info('LipNet is an end-to-end deep learning model designed for lipreading, which aims to transcribe speech solely based on visual information from lip movements. The model consists of three main components: a 3D Convolutional Neural Network (CNN), bidirectional LSTM layers, and a Connectionist Temporal Classification (CTC) layer.')
+    st.info('LipNet is an end-to-end deep learning model designed for lipreading, which aims to transcribe speech solely based on visual information from lip movements. The model consists of three main components: a 3D Convolutional Neural Network (CNN), bidirectional LSTM layers, and a Connectionist Temporal Classification (CTC) loss.')
 
 st.title('SilentScribe') 
 # Generating a list of options or videos 
